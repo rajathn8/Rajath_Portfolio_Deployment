e952ef24281a41b852244b937913dff18bcffa14
Rajath Rao <rajath@nextorbite.com>
2021-01-20 19:44:25 +0530

    Polling App Testss 2

---
+++ Portfolio/build/precache-manifest.43cc91bd86e4e1ef49cd7670b75e9bb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffa50f5eacd22f4f944e",
+    "url": "/me/static/css/main.3bc84f63.chunk.css"
+  },
+  {
+    "revision": "ffa50f5eacd22f4f944e",
+    "url": "/me/static/js/main.ffa50f5e.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "efa3de361c8fd8ee80a0b9d02b82a4d5",
+    "url": "/me/index.html"
+  }
+];
